c3f77d8e2025071a07c0de2c786f4439fae664aa
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-15 19:53:50 -0500

    add routes and controller methods and test in insomnia

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def show_one_product
+    render json: { message: "hello" }
+  end
+
+  def show_all_product
+    render json: { message: "goodbye" }
+  end
 end
