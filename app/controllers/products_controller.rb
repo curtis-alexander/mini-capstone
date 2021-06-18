8bc0f1abb27055a81414e4ae93d6f8d3736bdcf3
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-17 21:16:45 -0500

    add update action hardcode

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,15 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def update
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    product.name = "toy"
+    product.price = 3
+    product.image_url = ""
+    product.description = "loud annoying noise maker"
+    product.save
+    render json: product.as_json
+  end
 end
