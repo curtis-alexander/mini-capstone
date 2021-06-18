ac052b0255ca76ee6ba666ed1636bb367b6da488
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-17 20:03:41 -0500

    delete old actions and add new 'Roy' actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,22 @@
 class ProductsController < ApplicationController
-  def show_one_product
-    product = Product.first
-    render json: product.as_json
+  def index
+    products = Product.all
+    render json: products.as_json
   end
 
-  def show_all_product
-    product = Product.all
-    render json: product.as_json
+  def show
+    id = params[:id]
+    product = Product.find_by(id: id)
+    render json: product
   end
+
+  # def show_one_product
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
+
+  # def show_all_product
+  #   product = Product.all
+  #   render json: product.as_json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/one_product", controller: "products", action: "show_one_product"
-  get "/all_products", controller: "products", action: "show_all_product"
+
+  get "/products", controller: "products", action: "index"
+  get "/products/:id", controller: "products", action: "show"
 end
