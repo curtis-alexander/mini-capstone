edc487c44e2d6eb36a20022efdd04f7baf5d99bd
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-17 20:24:07 -0500

    add create action wet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,13 +10,14 @@ class ProductsController < ApplicationController
     render json: product
   end
 
-  # def show_one_product
-  #   product = Product.first
-  #   render json: product.as_json
-  # end
-
-  # def show_all_product
-  #   product = Product.all
-  #   render json: product.as_json
-  # end
+  def create
+    product = Product.new(
+      name: "bath bomb",
+      price: 10,
+      image_url: "https://www.lushusa.com/dw/image/v2/BDMQ_PRD/on/demandware.static/-/Sites-lushcosmetics-export/default/dwaa7b5b89/images/product/04031_a.jpg?sw=800&sh=800",
+      description: "circular device to make foamy bath water",
+    )
+    product.save
+    render json: product.as_json
+  end
 end
