04b1e63c83e483e1a1e86515c98b2921ba67f973
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-20 11:21:16 -0500

    add cors gem and test front end functions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,10 +12,10 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-      name: params[:input_name],
-      price: params[:input_price],
-      image_url: params[:input_image_url],
-      description: params[:input_description],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
     )
     product.save
     render json: product.as_json
@@ -24,10 +24,10 @@ class ProductsController < ApplicationController
   def update
     the_id = params[:id]
     product = Product.find_by(id: the_id)
-    product.name = params[:input_name]
-    product.price = params[:input_price]
-    product.image_url = params[:input_image_url]
-    product.description = params[:input_description]
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
     product.save
     render json: product.as_json
   end
