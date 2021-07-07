edb2862f554dff66fdeaee896c4ebe72266dafad
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-06 20:55:20 -0500

    create carted_products index

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -2,14 +2,20 @@ class CartedProductsController < ApplicationController
   def create
     carted_product = CartedProduct.new(
       product_id: params[:product_id],
-      user_id: params[:user_id],
+      user_id: current_user.id,
       quantity: params[:quantity],
-      status: params[:status],
+      status: "carted",
+      order_id: nil,
     )
     if carted_product.save
       render json: carted_product.as_json
     else
       render json: { error: carted_product.errors.full_messages }
     end
+  end
+
+  def index
+    carted_products = CartedProduct.all
+    render json: carted_products.as_json
   end
 end
