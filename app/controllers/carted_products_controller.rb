90f39e9a4eb33228ea77fac2f5f9a139c8f3d3c8
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-06 20:37:01 -0500

    make carted_products create action dynamic with params

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,15 @@
 class CartedProductsController < ApplicationController
+  def create
+    carted_product = CartedProduct.new(
+      product_id: params[:product_id],
+      user_id: params[:user_id],
+      quantity: params[:quantity],
+      status: params[:status],
+    )
+    if carted_product.save
+      render json: carted_product.as_json
+    else
+      render json: { error: carted_product.errors.full_messages }
+    end
+  end
 end
