b38945f083990e5a8139bf047331bc176e9c1acf
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-06 19:44:57 -0500

    create carted_products model and remove quantity and product_id from orders model

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :tax, :total, :is_discounted?, :inventory, :supplier, :images
+  attributes :id, :name, :price, :description, :tax, :total, :is_discounted?, :inventory
+  belongs_to :supplier
+  has_many :images
+  # has_many :categories
 end
---
+++ db/migrate/20210707003824_remove_tables_from_orders.rb
@@ -0,0 +1,6 @@
+class RemoveTablesFromOrders < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :orders, :quantity
+    remove_column :orders, :product_id
+  end
+end
