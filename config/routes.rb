6bf2015a690e28d05f981389c62af96b3aaf9968
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-06 20:16:59 -0500

    create carted_products controller and add has_many belongs_to associations

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   post "/products", controller: "products", action: "create"
   patch "/products/:id", controller: "products", action: "update"
   delete "/products/:id", controller: "products", action: "destroy"
+
   post "/suppliers", controller: "suppliers", action: "create"
   patch "/suppliers/:id", controller: "suppliers", action: "update"
 
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
   get "/orders" => "orders#index"
